1817adc2eec65993564038d47c58eb87457e6a90
yorick2 <pau@junowebdesign.com>
2016-01-21 11:51:03 +0000

    work for none magento site

--- droplet_setup__workInProgress__.sh
+++ droplet_setup__workInProgress__.sh
@@ -14,6 +14,8 @@ if [ "$importgit" != "y" ] && [ "$importgit" != "n" ]  ; then
   exit ;
 fi
 
+
+########### import database ############
 echo 
 echo 'Import a database file [y/n]';
 read importsql ;
@@ -67,6 +69,8 @@ if [ "$importsql" = "y" ]  ; then
 	fi;
 fi;
 
+
+######### is it a dev site #######
 echo 
 echo 'developement site? [y/n]' ;
 read dev ;
@@ -76,20 +80,30 @@ if [ "$dev" != "y" ] && [ "$dev" != "n" ] ; then
   exit ;
 fi;
 
+###### is it a magento site #####
+echo 
+echo 'magento site? [y/n]' ;
+read magento ;
+# if dev not set right ;
+if [ "$magento" != "y" ] && [ "$magento" != "n" ] ; then
+  echo 'not valid answer please try again';
+  exit ;
+fi;
 
-# make main structure (without web folder)
+####### make main structure (without web folder) ########
 echo '-->making main folder (without web folder)';
 mkdir -p "/var/www/${url}/";
 
 
-# setup vhost
+############ setup vhost #########
 echo '-->changing nginx webroot';
 sitesAvailibileFolder='/etc/nginx/sites-available';
 sudo sed -i "s/droplet/${url}/g"  "${sitesAvailibileFolder}/default"  ;
 sudo service nginx restart;
 
+
+######## git ########
 folder="/var/www/${url}/htdocs" ;
-# import git
 if [ "$importgit" = "y" ]  ; then
 	echo ;
 	echo 'What is the https: git repo url? (it has to be the https one)' ;
@@ -116,7 +130,7 @@ else
 	mkdir -p ${folder} ;
 fi;
 
-# database manipulation
+######## database manipulation ######
 if [ "$importsql" = "y" ]  ; then
 	# import db ;
 	echo '-->creating db';
@@ -135,41 +149,53 @@ if [ "$importsql" = "y" ]  ; then
 	mysql -u${mysqluser} -p${mysqlpassword} -e"${cmd}";
 fi;
 
+
+####### composer #######
 if [ "$isComposer" = "y" ] ; then
 	cd ${gitFolder} ;
 	composer install;
 	sudo chmod -R w+g var media;
 fi;
 
-echo '-->making var and media folders';
-mkdir -m 774 $folder/media;
-mkdir -m 774 $folder/var;
+if [ "$magento" = "y" ] ; then 
 
+	######## make media & var folders #######
+	echo '-->making var and media folders';
+	mkdir -m 774 $folder/media;
+	mkdir -m 774 $folder/var;
 
-echo '-->adding app/etc/local.xml';
-if [ "$importgit" = "n" ]  ; then
-	mkdir $folder/app ;
-	mkdir $folder/app/etc ;
-fi;
-if [ "$importsql" = "y" ]  ; then
-	cp "setup_files/local.xml" ${folder}/app/etc/local.xml ;
-	sudo sed -i "s/mysqluser/${mysqluser}/g"  "${folder}/app/etc/local.xml"  ;
-	sudo sed -i "s/mysqlpassword/${mysqlpassword}/g"  "${folder}/app/etc/local.xml"  ;
-	sudo sed -i "s/database_name/${db}/g"  "${folder}/app/etc/local.xml"  ;
+	####### app/etc/local.xml ######
+	# make file
+	echo '-->adding app/etc/local.xml';
+	if [ "$importgit" = "n" ]  ; then
+		mkdir $folder/app ;
+		mkdir $folder/app/etc ;
+	fi;
+	# update db
+	if [ "$importsql" = "y" ]  ; then
+		cp "setup_files/local.xml" ${folder}/app/etc/local.xml ;
+		sudo sed -i "s/mysqluser/${mysqluser}/g"  "${folder}/app/etc/local.xml"  ;
+		sudo sed -i "s/mysqlpassword/${mysqlpassword}/g"  "${folder}/app/etc/local.xml"  ;
+		sudo sed -i "s/database_name/${db}/g"  "${folder}/app/etc/local.xml"  ;
+	fi;
+
+	####### add standard htaccess #######
+	echo '-->adding .htaccess';
+	cp "setup_files/htaccess" ${folder}/.htaccess ;
 fi;
 
 if [ "$importgit" = "n" ]  ; then
 	echo "<?php echo 'your <strong>"${url}"</strong> droplet is setup'  ?>" > ${folder}/index.php ;
 fi;
 
-echo '-->adding .htaccess';
-cp "setup_files/htaccess" ${folder}/.htaccess ;
-
+####### add robots.txt #####
 if [ dev=='y'  ] ; then \
   	echo 'this is a dev site, we will add a robots.txt file'
 	echo '-->adding robots.txt';
 	cp "setup_files/robots.txt" ${folder}/robots.txt ;
 fi ;
 
+
+###### fin #######
 echo ------successfully completed------ ;
 echo 'WARNING: dont forget to check robots.txt can be seen by google, by going to '${url}'/robots.txt';
